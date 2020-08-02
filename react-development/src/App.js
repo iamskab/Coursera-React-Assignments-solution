e0c3dddca77c48fe1d93adbee3466da620ddca33
iamskab <iamsourav470@gmail.com>
2020-08-02 21:46:15 +0530

    Added Sols for Assignment-1 of week-1

---
+++ react-development/src/App.js
@@ -0,0 +1,35 @@
+import React , { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap' ;
+import Menu from './components/Menu' ;
+// import DishDetail from './components/DishDetail' ;
+import './App.css';
+import { DISHES } from './shared/dishes' ;
+
+
+
+class App extends Component {
+  constructor(props){
+    super(props) ;
+    this.state={
+      dishes:DISHES 
+    };
+  }
+  render(){
+    return (
+      <div >
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">
+              A Navbar Brand Item 
+            </NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+        {/* <DishDetail dishes={this.state.dishes}/> */}
+
+      </div>
+    );
+  }
+  }
+
+export default App;
